7c491bb90aecd3367481b43ee0fe677f896e37b2
Will Dickson <will@iorodeo.com>
2017-01-16 12:51:31 -0800

    Added CircularBuffer.

---
+++ firmware/devel/ps_circular_buffer.h
@@ -0,0 +1,17 @@
+#ifndef PS_CIRCULAR_BUFFER_H
+#define PS_CIRCULAR_BUFFER_H
+
+namespace ps
+{
+    class CircularBuffer
+    {
+
+        public:
+
+        protected:
+
+    };
+
+}
+
+#endif
---
+++ firmware/devel/ps_range_defs.h
@@ -0,0 +1,5 @@
+namespace ps
+{
+
+
+} // namespace ps
