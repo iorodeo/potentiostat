cc77da8905aa7f62063507b72d8a3b2f03e3c9f8
Will Dickson <will@iorodeo.com>
2017-01-16 15:34:23 -0800

    Finished initial version of CircularBuffer.

--- firmware/devel/ps_circular_buffer.h
+++ firmware/devel/ps_circular_buffer.h
@@ -1,17 +1,153 @@
 #ifndef PS_CIRCULAR_BUFFER_H
 #define PS_CIRCULAR_BUFFER_H
+#include <Arduino.h>
+#include "Array.h"
 
 namespace ps
 {
+    template<typename T, size_t MAX_SIZE>
     class CircularBuffer
     {
 
         public:
 
+            CircularBuffer();
+            T& front();
+            T& back();
+            void push_back(const T &value);
+            void push_front(const T &value);
+            void pop_front();
+            void pop_back();
+
+            size_t size();
+            size_t max_size();
+
+            size_t pos_front();
+            size_t pos_back();
+
         protected:
+
+            Array<T,MAX_SIZE> data_;
+
+            size_t  pos_front_ = 0;
+            size_t  pos_back_  = 0;
 
     };
 
-}
+
+    template<typename T, size_t MAX_SIZE>
+    CircularBuffer<T,MAX_SIZE>::CircularBuffer() 
+    {
+        T dummy = T();
+        data_.fill(dummy);
+    };
+
+
+    template<typename T, size_t MAX_SIZE>
+    T& CircularBuffer<T,MAX_SIZE>::front()
+    {
+        return data_[pos_front_];
+    }
+
+
+    template<typename T, size_t MAX_SIZE>
+    T& CircularBuffer<T,MAX_SIZE>::back()
+    {
+        return data_[pos_back_];
+    }
+
+
+    template<typename T, size_t MAX_SIZE>
+    void CircularBuffer<T,MAX_SIZE>::push_back(const T &value)
+    {
+        if (size() < (MAX_SIZE - 1))
+        {
+            pos_back_ = (pos_back_ + 1)%MAX_SIZE; 
+            data_[pos_back_] = value;
+        }
+    }
+
+
+    template<typename T, size_t MAX_SIZE>
+    void CircularBuffer<T,MAX_SIZE>::push_front(const T &value)
+    {
+        if (size() < (MAX_SIZE - 1))
+        {
+            if (pos_front_ > 0)
+            {
+                pos_front_--;
+            }
+            else
+            {
+                pos_front_ = MAX_SIZE-1;
+            }
+            data_[pos_front_] = value;
+        }
+    }
+
+
+    template<typename T, size_t MAX_SIZE>
+    void CircularBuffer<T,MAX_SIZE>::pop_front()
+    {
+        if (size() > 0)
+        {
+            pos_front_ = (pos_front_ + 1)%MAX_SIZE; 
+        }
+    }
+
+
+    template<typename T, size_t MAX_SIZE>
+    void CircularBuffer<T,MAX_SIZE>::pop_back()
+    {
+        if (size() > 0)
+        {
+            if (pos_back_ > 0)
+            {
+                pos_back_--;
+            }
+            else
+            {
+                pos_back_ = MAX_SIZE - 1;
+            }
+        }
+    }
+
+
+    template<typename T, size_t MAX_SIZE>
+    size_t CircularBuffer<T,MAX_SIZE>::size()
+    {
+        if (pos_front_ <= pos_back_)
+        {
+            return pos_back_ - pos_front_;
+        }
+        else
+        {
+            return MAX_SIZE - pos_front_ + pos_back_;
+        }
+    }
+
+
+    template<typename T, size_t MAX_SIZE>
+    size_t CircularBuffer<T,MAX_SIZE>::max_size()
+    {
+        return MAX_SIZE;
+    }
+
+
+    template<typename T, size_t MAX_SIZE>
+    size_t CircularBuffer<T,MAX_SIZE>::pos_front()
+    {
+        return pos_front_;
+    }
+
+
+    template<typename T, size_t MAX_SIZE>
+    size_t CircularBuffer<T,MAX_SIZE>::pos_back()
+    {
+        return pos_back_;
+    }
+
+
+} // namespace ps
 
 #endif
--- firmware/devel/ps_range_defs.h
+++
@@ -1,5 +0,0 @@
-namespace ps
-{
-
-
-} // namespace ps
