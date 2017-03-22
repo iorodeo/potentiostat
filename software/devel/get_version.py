b64df228d243e2e9df905d41686eb658ebf0f11c
Will Dickson <will@iorodeo.com>
2017-03-21 18:01:45 -0700

    Removed old devel directory.

--- software/devel/get_version.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-from potentiostat import Potentiostat
-
-port = '/dev/ttyACM0'
-dev = Potentiostat(port)
-
-version = dev.get_version()
-print('firmware version: ', version)
