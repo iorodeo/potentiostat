3504f7d044b040ba301db1d31458658d2597e9d5
Will Dickson <will@iorodeo.com>
2018-08-21 16:53:54 -0700

    Added multiplexer pins to pin map.

--- firmware/libraries/potentiostat/ps_pin_map.h
+++ firmware/libraries/potentiostat/ps_pin_map.h
@@ -21,6 +21,25 @@ namespace ps
     const int DAC_UNI_PIN = A14;
     const int TIA_OUT_UNI_PIN = A1;
     const int REF_ELECT_UNI_PIN = A2;
+
+    // Multiplexer switch pins
+    const int MUX_WRK1_TO_WRK = 24;
+    const int MUX_WRK1_TO_GND = 28;
+    const int MUX_WRK2_TO_WRK = 25;
+    const int MUX_WRK2_TO_GND = 29;
+    const int MUX_WRK3_TO_WRK = 26;
+    const int MUX_WRK3_TO_GND = 30;
+    const int MUX_WRK4_TO_WRK = 27;
+    const int MUX_WRK4_TO_GND = 31;
+    const int MUX_WRK5_TO_WRK = 4;
+    const int MUX_WRK5_TO_GND = 11;
+    const int MUX_WRK6_TO_WRK = 19;
+    const int MUX_WRK6_TO_GND = 13;
+    const int MUX_WRK7_TO_WRK = 18;
+    const int MUX_WRK7_TO_GND = 10;
+    const int MUX_CTR_CONN = 3;
+    const int MUX_REF_CONN = 12;
+
 }
 
 #endif
