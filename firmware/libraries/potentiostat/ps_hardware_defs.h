424bc13d119375e098f73c06fef63cb351e20f10
Will Dickson <will@iorodeo.com>
2023-03-16 10:57:11 -0700

    Cleaned up ps_hardware_defs.h

--- firmware/libraries/potentiostat/ps_hardware_defs.h
+++ firmware/libraries/potentiostat/ps_hardware_defs.h
@@ -1,7 +1,7 @@
 #ifndef PS_HARDWARE_DEFS_H
 #define PS_HARDWARE_DEFS_H
     
-    // Select hardware version
+    // Select your hardware version
     // ---------------------------------------------------------------
      #define HW0P2_ITSY_BITSY_10V_MICR0
     // #define HW0P2_ITSY_BITSY_10V_MILL10
@@ -67,29 +67,8 @@
         #error "no hardware version selected must select a hardware verion"
 
     #endif
+
     
-    // Select hardware version
-    // ------------------------------------------------------------------------------
-    //#define HARDWARE_VERSION_0P2   // select from (VERSION_0P1 OR VERSION_0P2)
-    //#if defined(HARDWARE_VERSION_0P1) && defined(HARDWARE_VERSION_0P2)
-    //    #error "only one hardware version can be defined"
-    //#endif
-
-    //// Options based on hardware version 
-    //#if defined HARDWARE_VERSION_0P2 
-    //    #define VOLTAGE_VARIANT_10V          // select from (10V)   
-    //    //#define CURRENT_VARIANT_MILL10_AMP   // select from (MICRO, MILL10, MILL24)
-    //    #define CURRENT_VARIANT_MICRO_AMP    // select from (MICRO, MILL10, MILL24)
-    //    #define DEVBOARD_ITSY_BITSY          // select from (TEENSY or ITSY_BITSY)
-    //    //#define DEVBOARD_TEENSY             // select from (TEENSY)
-    //#elif defined HARDWARE_VERIONS_0P1 
-    //    #define VOLTAGE_VARIANT_AD8250      // select from (AD8250 or AD8251)
-    //    #define CURRENT_VARIANT_MICRO_AMP   // select from (NANO, MICRO or MILL24)
-    //    #define DEVBOARD_TEENSY             // select from (TEENSY)
-    //#else
-    //    #error "must define hardware verion"
-    //#endif
-
     #if defined DEVBOARD_TEENSY
         #define MUX_CAPABLE 
     #endif
